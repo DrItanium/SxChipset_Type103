8c72277c8896611393f0579f3390da287da2fac5
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-01-05 21:41:39 -0800

    Define address translation initial concept

---
+++ Chipset/include/AddressTranslation.h
@@ -0,0 +1,45 @@
+/*
+i960SxChipset_Type103
+Copyright (c) 2022-2023, Joshua Scoggins
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+#ifndef SXCHIPSET_ADDRESS_TRANSLATION_H__
+#define SXCHIPSET_ADDRESS_TRANSLATION_H__
+#include "Detect.h"
+#include "Types.h"
+
+/**
+ * @brief Converts virtual addresses to physical addresses
+ */
+class AddressTranslator {
+    public:
+        AddressTranslator() = delete;
+        ~AddressTranslator() = delete;
+        AddressTranslator(const AddressTranslator&) = delete;
+        AddressTranslator(AddressTranslator&&) = delete;
+        AddressTranslator& operator=(const AddressTranslator&) = delete;
+        AddressTranslator& operator=(AddressTranslator&&) = delete;
+};
+
+#endif // end !defined(SXCHIPSET_ADDRESS_TRANSLATION_H__)

8a2cd245e98a64fe61ba315561dfc433a429714d
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-01-07 17:06:08 -0800

    Use a 2-way lru implementation

--- Chipset/include/Cache.h
+++ Chipset/include/Cache.h
@@ -388,7 +388,7 @@ private:
     DataCacheLine cache[NumberOfSets];
 };
 
-using MemoryCache = BasicDataCache<5, 8, 0, 1>;
+using MemoryCache = BasicDataCache<5, 7, 0, 2>;
 
 MemoryCache& getCache() noexcept;
 void setupCache() noexcept;

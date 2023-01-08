83bde45115e72b72d43b899d5c7c00b2d8531691
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-01-07 22:26:32 -0800

    Describe i960 data structures so the microcontroller could theoretically constructor or read them

--- Chipset/include/Boot960.h
+++ Chipset/include/Boot960.h
@@ -34,18 +34,11 @@ struct FaultRecord {
     uint32_t pc;
     uint32_t ac;
     uint32_t type;
-    uint32_t* addr;
+    uint32_t addr;
 } __attribute((packed));
 struct FaultTableEntry {
-    typedef void(*FaultOperation)();
     uint32_t handlerRaw;
     int32_t magicNumber;
-    inline FaultOperation getFaultFunction() const noexcept { return reinterpret_cast<FaultOperation>(handlerRaw & 0xFFFFFFFC); }
-    inline uint8_t getProcedureKind() const noexcept { return static_cast<uint8_t>(handlerRaw & 0x3); }
-    inline bool isLocalProcedure() const noexcept { return getProcedureKind() == 0; }
-    inline bool isSystemProcedure() const noexcept { return getProcedureKind() == 0x2 && getMagicNumber() == 0x0000027F; }
-    inline bool isTraceFaultHandler() const noexcept { return getProcedureKind() == 0x2 && getMagicNumber() == 0x000002BF; }
-    inline int32_t getMagicNumber() const noexcept { return magicNumber; }
 } __attribute((packed));
 struct FaultTable {
     FaultTableEntry entries[32];
@@ -53,90 +46,58 @@ struct FaultTable {
 struct InterruptTable {
     uint32_t pendingPriorities;
     uint32_t pendingInterrupts[8];
-    void (*interruptProcedures[248])();
+    uint32_t interruptProcedures[248];
 } __attribute((packed));
 struct InterruptRecord {
     uint32_t pc;
     uint32_t ac;
     uint8_t vectorNumber;
 } __attribute((packed));
-struct SATEntry {
-    typedef void (*Executable)();
-    enum Kind {
-        LocalProcedure,
-        Reserved0,
-        SupervisorProcedure,
-        Reserved1,
-        Invalid = Reserved1,
-    };
-    uint32_t raw;
-    inline Executable asFunction() const noexcept {
-        // drop the lowest tag bits to make sure that it is the right location
-        return reinterpret_cast<Executable>(raw & 0xFFFFFFFC);
-    }
-    inline Kind getKind() const noexcept {
-        return static_cast<Kind>(raw & 0x3);
-    }
-} __attribute((packed));
 struct SysProcTable {
     uint32_t reserved[3];
-    void* supervisorStack;
+    uint32_t supervisorStack;
     uint32_t preserved[8];
-    SATEntry entries[260];
-    typedef SATEntry::Executable FunctionBody;
-
-    inline FunctionBody getEntry(uint32_t index) noexcept {
-        if (index > 259) {
-            return nullptr;
-        } else {
-            return entries[index].asFunction();
-        }
-    }
-    inline SATEntry::Kind getEntryKind(uint32_t index) noexcept {
-        if (index > 259) {
-            return SATEntry::Reserved1;
-        } else {
-            return entries[index].getKind();
-        }
-    }
+    uint32_t entries[260];
 } __attribute((packed));
 struct PRCB {
     uint32_t reserved0;
     uint32_t magicNumber;
     uint32_t reserved1[3];
-    InterruptTable* theInterruptTable;
-    void* interruptStack;
+    uint32_t theInterruptTable;
+    uint32_t interruptStack;
     uint32_t reserved2;
     uint32_t magicNumber1;
     uint32_t magicNumber2;
-    FaultTable* theFaultTable;
+    uint32_t theFaultTable;
     uint32_t reserved3[32];
 } __attribute((packed));
-/**
- * @brief Describes an i960Sx boot structure that can be embedded into flash
- */
-struct SystemAddressTable {
-    SystemAddressTable* satPtr;
-    PRCB* thePRCB;
+struct [[gnu::packed]] SystemAddressTable {
+    uint32_t satPtr;
+    uint32_t thePRCB;
     uint32_t checkWord;
-    void (*firstInstruction)();
+    uint32_t firstInstruction;
     uint32_t checkWords[4];
     uint32_t preserved0[22];
-    SysProcTable* theProcTable0;
+    uint32_t theProcTable0;
     uint32_t magicNumber0;
     uint32_t preserved1[2];
-    SystemAddressTable* offset0;
+    uint32_t offset0;
     uint32_t magicNumber1;
     uint32_t preserved2[2];
-    SysProcTable* theProcTable1;
+    uint32_t theProcTable1;
     uint32_t magicNumber2;
     uint32_t preserved3[2];
-    SysProcTable* traceTable;
+    uint32_t traceTable;
     uint32_t magicNumber3;
-} __attribute((packed));
+};
+static_assert(sizeof(SystemAddressTable) == (32 + 88 + 16 + 16 + 16 + 8));
 constexpr uint32_t computeCS1(uint32_t sat, uint32_t prcb, uint32_t first_ip) noexcept {
     return -(sat + prcb + first_ip);
 }
+
+/**
+ * @brief Describes an i960Sx boot structure that can be embedded into flash
+ */
 struct [[gnu::packed]] CoreInitializationBlock {
     explicit constexpr CoreInitializationBlock(uint32_t satBase, uint32_t prcbPointer, uint32_t checkWord, uint32_t firstIP) :
             sat_(satBase),

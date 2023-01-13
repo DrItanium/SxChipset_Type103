89db4d474518e648b3d60c89d350cc8aebf45ccb
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-01-13 13:36:01 -0800

    Only enable toggle mode when we set the system prescalar

--- Chipset/src/RTCDevice.cc
+++ Chipset/src/RTCDevice.cc
@@ -42,9 +42,6 @@ TimerDevice::init() noexcept {
     // make sure that INT0 is enabled as an output. Make it high
     pinMode<Pin::INT0_960_>(OUTPUT);
 #if defined(TYPE103_BOARD) || defined(TYPE104_BOARD) || defined(TYPE203_BOARD)
-    // enable toggle mode
-    bitSet(TCCR2A, COM2A0);
-    bitClear(TCCR2A, COM2A1);
     // enable CTC (OCR2A) mode
     bitClear(TCCR2A, WGM20);
     bitSet(TCCR2A, WGM21);
@@ -85,8 +82,17 @@ TimerDevice::extendedWrite(const Channel0Value& m0, uint16_t value) noexcept {
         case TimerDeviceOperations::SystemTimerPrescalar: 
 #if defined(TYPE103_BOARD) || defined(TYPE104_BOARD)  || defined(TYPE203_BOARD)
             {
+                // enable toggle mode
+                auto maskedValue = value & 0b111;
+                if (value != 0) {
+                    bitSet(TCCR2A, COM2A0);
+                    bitClear(TCCR2A, COM2A1);
+                } else {
+                    bitClear(TCCR2A, COM2A0);
+                    bitClear(TCCR2B, COM2A1);
+                }
                 uint8_t result = TCCR2B & 0b1111'1000;
-                result |= static_cast<uint8_t>(value & 0b111);
+                result |= static_cast<uint8_t>(maskedValue);
                 TCCR2B = result;
                 break;
             }

7f2e24024becf3449a4f5ef8c5b3ad9ea84a5644
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-01-14 14:25:24 -0800

    Hook up cache interface through the pool interface
    Don't use the cache as an onchip setup

--- Chipset/include/Cache.h
+++ Chipset/include/Cache.h
@@ -30,6 +30,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define CHIPSET_CACHE_H
 #include "Detect.h"
 #include "Types.h"
+#include "xmem.h"
 constexpr bool EnableCacheDebugging = false;
 size_t memoryWrite(SplitWord32 baseAddress, uint8_t* bytes, size_t count) noexcept;
 size_t memoryRead(SplitWord32 baseAddress, uint8_t* bytes, size_t count) noexcept;
@@ -345,12 +346,6 @@ struct BasicDataCache {
             set.begin();
         }
     }
-    [[nodiscard]] byte* asBuffer() noexcept {
-        return reinterpret_cast<byte*>(cache);
-    }
-    [[nodiscard]] constexpr size_t sizeOfBuffer() const noexcept {
-        return sizeof(cache);
-    }
 private:
     DataCacheSet cache[NumberOfSets];
 };
@@ -378,17 +373,129 @@ struct BasicDataCache<offsetBits, tagBits, bankBits, 1> {
             set.begin();
         }
     }
-    [[nodiscard]] byte* asBuffer() noexcept {
-        return reinterpret_cast<byte*>(cache);
-    }
-    [[nodiscard]] constexpr size_t sizeOfBuffer() const noexcept {
-        return sizeof(cache);
-    }
 private:
     DataCacheLine cache[NumberOfSets];
 };
+
+template<uint8_t offsetBits, uint8_t tagBits, uint8_t bankBits, uint8_t numberOfLines>
+struct BasicCacheReference {
+    using Cache = BasicDataCache<offsetBits, tagBits, bankBits, numberOfLines>;
+#ifdef TYPE203_BOARD
+    static_assert(sizeof(Cache) < 32767, "Cache implementation is too large to fit in a 32k block");
+#endif
+    using DataCacheLine = typename Cache::DataCacheLine;
+    using CacheAddress = typename Cache::CacheAddress;
+    void select() {
+        if (initialized_) {
+            xmem::setMemoryBank(index_);
+        }
+    }
+    void begin(byte index) noexcept {
+        if (!initialized_) {
+            Serial.print(F("Sizeof cache = "));
+            Serial.println(sizeof(Cache));
+            initialized_ = true;
+            index_ = index;
+            select();
+            ptr_ = new Cache();
+            ptr_->begin();
+        }
+    }
+    auto& find(CacheAddress addr, bool skipLoadingFromMainMemory = false) noexcept {
+        // make sure we select this bank before we jump to the pointer
+        select();
+        return ptr_->find(addr, skipLoadingFromMainMemory);
+    }
+    void clear() noexcept {
+        select();
+        ptr_->clear();
+    }
+private:
+    bool initialized_ = false;
+    byte index_ = 0;
+    Cache* ptr_ = nullptr;
+};
+
+template<uint8_t offsetBits, uint8_t tagBits, uint8_t numberOfLines>
+struct BasicCacheReference<offsetBits, tagBits, 0, numberOfLines> {
+    using Cache = BasicDataCache<offsetBits, tagBits, 0, numberOfLines>;
+    using DataCacheLine = typename Cache::DataCacheLine;
+    using CacheAddress = typename Cache::CacheAddress;
+    void begin() noexcept {
+        if (!initialized_) {
+            Serial.print(F("Sizeof cache = "));
+            Serial.println(sizeof(Cache));
+            initialized_ = true;
+            ptr_.begin();
+        }
+    }
+    inline auto& find(CacheAddress addr, bool skipLoadingFromMainMemory = false) noexcept {
+        // make sure we select this bank before we jump to the pointer
+        return ptr_.find(addr, skipLoadingFromMainMemory);
+    }
+    void clear() noexcept {
+        ptr_.clear();
+    }
+private:
+    bool initialized_ = false;
+    Cache ptr_;
+};
+template<uint8_t offsetBits, uint8_t tagBits, uint8_t bankBits, uint8_t numberOfLines>
+struct CachePool {
+    using CacheReference = BasicCacheReference<offsetBits, tagBits, bankBits, numberOfLines>;
+    using CacheAddress = typename CacheReference::CacheAddress;
+    using CacheLine = typename CacheReference::DataCacheLine;
+    static constexpr auto NumberOfBanks = pow2(bankBits);
+    void begin(byte bankOffset) noexcept {
+        if (!initialized_) {
+            initialized_ = true;
+            bankOffset_ = bankOffset;
+            for (int i = 0, j = bankOffset_; i < NumberOfBanks; ++i, ++j) {
+                pool_[i].begin(j);
+            }
+        }
+    }
+    inline auto& find(const SplitWord32& address, bool skipLoadingFromMainMemory = false) noexcept {
+        CacheAddress addr(address);
+        return pool_[addr.getBankIndex()].find(addr, skipLoadingFromMainMemory);
+    }
+    void clear()  {
+        for (int i = 0; i < NumberOfBanks; ++i) {
+            pool_[i].clear();
+        }
+    }
+private:
+    bool initialized_ = false;
+    byte bankOffset_ = 0;
+    CacheReference pool_[NumberOfBanks];
+};
+
+template<uint8_t offsetBits, uint8_t tagBits, uint8_t numberOfLines>
+struct CachePool<offsetBits, tagBits, 0, numberOfLines> {
+    using CacheReference = BasicCacheReference<offsetBits, tagBits, 0, numberOfLines>;
+    using CacheAddress = typename CacheReference::CacheAddress;
+    using CacheLine = typename CacheReference::DataCacheLine;
+    static constexpr auto NumberOfBanks = pow2(0);
+    void begin() noexcept {
+        if (!initialized_) {
+            initialized_ = true;
+            pool_.begin();
+        }
+    }
+    inline auto& find(const SplitWord32& address, bool skipLoadingFromMainMemory = false) noexcept {
+        CacheAddress addr(address);
+        return pool_.find(addr, skipLoadingFromMainMemory);
+    }
+    void clear()  {
+        pool_.clear();
+    }
+private:
+    bool initialized_ = false;
+    CacheReference pool_;
+};
 #if defined(TYPE103_BOARD) || defined(TYPE104_BOARD)
-using MemoryCache = BasicDataCache<4, 8, 0, 2>;
+//using MemoryCache = BasicDataCache<4, 8, 0, 2>;
+using MemoryCache = CachePool<4, 8, 0, 2>;
 #elif defined(TYPE203_BOARD) || defined(TYPE200_BOARD)
 using MemoryCache = BasicDataCache<4, 7, 0, 2>;
 #else

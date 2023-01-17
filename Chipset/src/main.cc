26a7ffa0dcad79729394808f6de1c0992740030a
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-01-16 16:49:50 -0800

    Start defining the banked memory storage io device
    It is going to be very simple overall

--- Chipset/src/main.cc
+++ Chipset/src/main.cc
@@ -35,7 +35,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "InfoDevice.h"
 #include "Cache.h"
 #include "Boot960.h"
-
+#include "InternalStorageIO.h"
 SdFat SD;
 // the logging shield I'm using has a DS1307 RTC
 SerialDevice theSerial;

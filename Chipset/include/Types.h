c02d85bd5f9586b1cbeef253051eb153aba868b4
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-01-24 17:14:46 -0800

    Make sure we are shifting correctly when getting the bank address!

--- Chipset/include/Types.h
+++ Chipset/include/Types.h
@@ -120,8 +120,8 @@ union SplitWord32 {
         return static_cast<uintptr_t>(0x8000 | (halves[0]));
     }
     [[nodiscard]] constexpr uint24_t compute328BusBank() const noexcept {
-        return (static_cast<uint24_t>(halves[1]) << 1) | (bytes[0] & 0x80);
-        //return static_cast<uint24_t>(full >> 17);
+        //return (static_cast<uint24_t>(halves[1]) << 1) | (bytes[0] & 0x80);
+        return static_cast<uint24_t>(full >> 15);
     }
     void assignHalf(byte offset, uint16_t value) noexcept { halves[offset & 0b1] = value; }
     void clear() noexcept { full = 0; }

963582a09869a38e753fe813e20c91a8091a8378
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-01-24 17:36:59 -0800

    Allow me to select the correct action style on writes (either use a switch statement or conditional checks)

--- Chipset/src/main.cc
+++ Chipset/src/main.cc
@@ -273,7 +273,7 @@ talkToi960(const SplitWord32& addr, T& handler) noexcept {
 
 struct TreatAsOnChipAccess final { };
 
-template<bool isReadOperation>
+template<bool isReadOperation, bool useSwitchStatement = false>
 //[[gnu::noinline]]
 [[gnu::always_inline]]
 inline void
@@ -286,33 +286,33 @@ manipulateDataLines(SplitWord16* ptr) noexcept {
         // keep setting the data lines and inform the i960
         Platform::setDataLines(ptr->full);
     } else {
-#if 0
-        switch (readInputChannelAs<Channel0Value, false>().getByteEnable()) {
-            case EnableStyle::Full16:
-                ptr->full = Platform::getDataLines();
-                break;
-            case EnableStyle::Lower8:
-                // directly read from the ports to speed things up
-                ptr->bytes[0] = getInputRegister<Port::DataLower>();
-                break;
-            case EnableStyle::Upper8:
-                ptr->bytes[1] = getInputRegister<Port::DataUpper>();
-                break;
-            default:
-                break;
-        }
-#else
         if constexpr (EnableDebugMode) {
             Serial.print(F("\tWrite in 0x"));
             Serial.println(Platform::getDataLines(), HEX);
         }
-        if (digitalRead<Pin::BE0>() == LOW) {
-            ptr->bytes[0] = getInputRegister<Port::DataLower>();
+        if constexpr (useSwitchStatement) {
+            switch (static_cast<EnableStyle>(readInputChannelAs<Channel0Value, false>() & 0b11)) {
+                case EnableStyle::Full16:
+                    ptr->full = Platform::getDataLines();
+                    break;
+                case EnableStyle::Lower8:
+                    // directly read from the ports to speed things up
+                    ptr->bytes[0] = getInputRegister<Port::DataLower>();
+                    break;
+                case EnableStyle::Upper8:
+                    ptr->bytes[1] = getInputRegister<Port::DataUpper>();
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            if (digitalRead<Pin::BE0>() == LOW) {
+                ptr->bytes[0] = getInputRegister<Port::DataLower>();
+            }
+            if (digitalRead<Pin::BE1>() == LOW) {
+                ptr->bytes[1] = getInputRegister<Port::DataUpper>();
+            }
         }
-        if (digitalRead<Pin::BE1>() == LOW) {
-            ptr->bytes[1] = getInputRegister<Port::DataUpper>();
-        }
-#endif
     }
 }
 

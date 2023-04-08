30a6d8ee81b16504e04e785f7385dba99543fd8c
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-04-08 14:48:42 -0700

    Compact the code to make it easier to reason about
    I believe it is fine to reduce the space consumed as the actions will
    still be the same.

--- Chipset/src/main.cc
+++ Chipset/src/main.cc
@@ -505,6 +505,7 @@ public:
                         } \
                     } else { \
                         if (digitalRead<Pin:: BE ## d0 >() == LOW) { \
+                            theBytes[b0] = dataLines[d0]; \
                             if (digitalRead<Pin:: BE ## d1 > () == LOW) { \
                                 /* Keep in contiguous sequences to allow the
                                  * cpu to chain into the next operation. 
@@ -513,10 +514,8 @@ public:
                                  * AVR core will chain the operations using the
                                  * pipeline so there is no downtime. 
                                  */ \
-                                theBytes[b0] = dataLines[d0]; \
                                 theBytes[b1] = dataLines[d1]; \
                             } else { \
-                                theBytes[b0] = dataLines[d0]; \
                                 break; \
                             } \
                         } else { \

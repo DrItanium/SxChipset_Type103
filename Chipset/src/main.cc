5e93f09e6d768129a3c84577afea303bb7d6c5a9
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-04-11 09:46:31 -0700

    Activate the RTC but do nothing else for now

--- Chipset/src/main.cc
+++ Chipset/src/main.cc
@@ -34,6 +34,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "TimerDevice.h"
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
+#include <RTClib.h>
 
 SerialDevice theSerial;
 TimerDevice timerInterface;
@@ -41,6 +42,44 @@ TimerDevice timerInterface;
 Adafruit_ILI9341 tft(
         static_cast<uint8_t>(Pin::TFTCS),
         static_cast<uint8_t>(Pin::TFTDC));
+
+RTC_PCF8523 rtc;
+bool rtcAvailable = false;
+void
+setupRTC() noexcept {
+    rtcAvailable = rtc.begin();
+    if (!rtcAvailable) {
+        Serial.println(F("NO PCF8523 RTC found!"));
+    } else {
+        Serial.println(F("Found PCF8523 RTC!"));
+        if (!rtc.initialized() || rtc.lostPower()) {
+            Serial.println(F("RTC is not initialized, setting time!"));
+            rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+        }
+
+        rtc.start();
+        Serial.println(F("Enabling calibration to prevent rtc drift of 43 seconds in 1 week!"));
+        // taken from the pcf8523.ino example sketch:
+        // The PCF8523 can be calibrated for:
+        //        - Aging adjustment
+        //        - Temperature compensation
+        //        - Accuracy tuning
+        // The offset mode to use, once every two hours or once every minute.
+        // The offset Offset value from -64 to +63. See the Application Note for calculation of offset values.
+        // https://www.nxp.com/docs/en/application-note/AN11247.pdf
+        // The deviation in parts per million can be calculated over a period of observation. Both the drift (which can be negative)
+        // and the observation period must be in seconds. For accuracy the variation should be observed over about 1 week.
+        // Note: any previous calibration should cancelled prior to any new observation period.
+        // Example - RTC gaining 43 seconds in 1 week
+        float drift = 43; // seconds plus or minus over oservation period - set to 0 to cancel previous calibration.
+        float period_sec = (7 * 86400);  // total obsevation period in seconds (86400 = seconds in 1 day:  7 days = (7 * 86400) seconds )
+        float deviation_ppm = (drift / period_sec * 1000000); //  deviation in parts per million (μs)
+        float drift_unit = 4.34; // use with offset mode PCF8523_TwoHours
+                                 // float drift_unit = 4.069; //For corrections every min the drift_unit is 4.069 ppm (use with offset mode PCF8523_OneMinute)
+        int offset = round(deviation_ppm / drift_unit);
+        rtc.calibrate(PCF8523_TwoHours, offset); // Un-comment to perform calibration once drift (seconds) and observation period (seconds) are correct
+    }
+}
 
 void 
 putCPUInReset() noexcept {
@@ -1203,9 +1242,11 @@ setup() {
     setupPins();
     theSerial.begin();
     timerInterface.begin();
+    Wire.begin();
     SPI.begin();
     SPI.beginTransaction(SPISettings(F_CPU / 2, MSBFIRST, SPI_MODE0)); // force to 10 MHz
     setupDisplay();
+    setupRTC();
     // setup the IO Expanders
     Platform::begin();
     delay(1000);

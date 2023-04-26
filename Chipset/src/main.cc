7ba195496e4b7432bd0be3e51033a9b93f6f9605
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-04-26 13:00:31 -0700

    Document why we are disabling block reordering

--- Chipset/src/main.cc
+++ Chipset/src/main.cc
@@ -515,6 +515,9 @@ public:
 #define LO(b0, b1, later) X(0, b0, 1, b1, later)
 #define HI(b0, b1, later) X(2, b0, 3, b1, later)
 #pragma GCC push_options
+// block reordering causes expensive jumps to be used under the guise of
+// optimization. Turning it off for this function seems to improve performance
+// quite a bit
 #pragma GCC optimize ("no-reorder-blocks")
     [[gnu::always_inline]]
     inline

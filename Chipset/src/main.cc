f88a8f9571c814f280bbdb0f1d56a8b4f7307127
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-07-28 19:25:37 -0700

    Reads should always operate on 32-bit wide operations
    More likely than not, the operations will be properly aligned and as
    such 32-bit reads make a lot more sense. There will be some wasted time
    in other cases but who cares since we can quickly denote the next one
    with very little concern. It seems to actually improve the speed of
    flops.c by a meaningful portion (at least 0.0200 mflops in some cases).

--- Chipset/src/main.cc
+++ Chipset/src/main.cc
@@ -593,13 +593,48 @@ public:
             // because we are operating on 16-byte windows, there is no way
             // that we would ever fully access the entire 16-bytes in a single
             // transaction if we started unaligned like this
-            HI(0, 1, false, false);
-            LO(2, 3, true, false);
-            HI(4, 5, true, false);
-            LO(6, 7, true, false);
-            HI(8, 9, true, false);
-            LO(10, 11, true, false);
-            HI(12, 13, true, false);
+            if constexpr (isReadOperation) {
+                setDataByte(theBytes[2], theBytes[3], theBytes[0], theBytes[1]);
+                if (isBurstLast()) {
+                    goto Done; 
+                }
+                signalReady<true>();
+                if (isBurstLast()) {
+                    goto Done; 
+                }
+                signalReady<false>();
+                setDataByte(theBytes[6], theBytes[7], theBytes[4], theBytes[5]);
+                if (isBurstLast()) {
+                    goto Done; 
+                }
+                signalReady<true>();
+                if (isBurstLast()) {
+                    goto Done; 
+                }
+                signalReady<false>();
+                setDataByte(theBytes[10], theBytes[11], theBytes[8], theBytes[9]);
+                if (isBurstLast()) {
+                    goto Done; 
+                }
+                signalReady<true>();
+                if (isBurstLast()) {
+                    goto Done; 
+                }
+                signalReady<false>();
+                setDataByte(theBytes[14], theBytes[15], theBytes[12], theBytes[13]);
+                if (isBurstLast()) {
+                    goto Done; 
+                }
+                signalReady<true>();
+            } else {
+                HI(0, 1, false, false);
+                LO(2, 3, true, false);
+                HI(4, 5, true, false);
+                LO(6, 7, true, false);
+                HI(8, 9, true, false);
+                LO(10, 11, true, false);
+                HI(12, 13, true, false);
+            }
         }
 #undef LO
 #undef HI

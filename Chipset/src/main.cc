fb4c68c11ea85314eb0b73f599bedac07d179f4b
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-09-14 14:42:27 -0700

    ControlSignals are now being put in compile time guards

--- Chipset/src/main.cc
+++ Chipset/src/main.cc
@@ -91,6 +91,8 @@ constexpr auto MaximumBootImageFileSize = 1024ul * 1024ul;
 constexpr uintptr_t MemoryWindowBaseAddress = SupportNewRAMLayout ? 0x8000 : 0x4000;
 constexpr uintptr_t MemoryWindowMask = MemoryWindowBaseAddress - 1;
 
+constexpr bool UseOldAuxHardwareBoard = true;
+
 static_assert((MemoryWindowMask == 0x7FFF || MemoryWindowMask == 0x3FFF), "MemoryWindowMask is not right");
 
 constexpr auto displayHasTouchScreen() noexcept {
@@ -635,11 +637,19 @@ X(5);
 
 void 
 putCPUInReset() noexcept {
-    ControlSignals.ctl.reset = LOW;
+    if constexpr (UseOldAuxHardwareBoard) {
+        ControlSignals.ctl.reset = LOW;
+    } else {
+        digitalWrite<Pin::RESET, LOW>();
+    }
 }
 void 
 pullCPUOutOfReset() noexcept {
-    ControlSignals.ctl.reset = HIGH;
+    if constexpr (UseOldAuxHardwareBoard) {
+        ControlSignals.ctl.reset = HIGH;
+    } else {
+        digitalWrite<Pin::RESET, HIGH>();
+    }
 }
 
 
@@ -1554,9 +1564,11 @@ executionBody() noexcept {
     // I want to use the upper four bits the XBUS address lines
     // while I can directly connect to the address lines, I want to test to
     // make sure that this works as well
-    ControlSignals.ctl.bankSelect = 1;
+    if constexpr (UseOldAuxHardwareBoard) {
+        ControlSignals.ctl.bankSelect = 1;
 
-    XBUSBankRegister.view32.data = 0;
+        XBUSBankRegister.view32.data = 0;
+    }
     // at this point, we are setup to be in output mode (or read) and that is the
     // expected state for _all_ i960 processors, it will load some amount of
     // data from main memory to start the execution process. 
@@ -1720,7 +1732,6 @@ setupPins() noexcept {
 }
 void 
 setupPlatform() noexcept {
-    static constexpr uint32_t ControlSignalDirection = 0b10000000'11111111'00111000'00010001;
     // setup the EBI
     XMCRB=0b1'0000'000;
     // use the upper and lower sector limits feature to accelerate IO space
@@ -1734,56 +1745,63 @@ setupPlatform() noexcept {
     AddressLinesInterface.view32.direction = 0;
     DataLinesInterface.view32.direction = 0xFFFF'FFFF;
     DataLinesInterface.view32.data = 0;
-    ControlSignals.view32.direction = ControlSignalDirection;
-    if constexpr (MCUHasDirectAccess) {
-        ControlSignals.view8.direction[1] &= 0b11101111;
+    if constexpr (UseOldAuxHardwareBoard) {
+        static constexpr uint32_t ControlSignalDirection = 0b10000000'11111111'00111000'00010001;
+        ControlSignals.view32.direction = ControlSignalDirection;
+        if constexpr (MCUHasDirectAccess) {
+            ControlSignals.view8.direction[1] &= 0b11101111;
+        }
+        if constexpr (XINT0DirectConnect) {
+            ControlSignals.view8.direction[2] &= 0b11111110;
+        }
+        if constexpr (XINT1DirectConnect) {
+            ControlSignals.view8.direction[2] &= 0b11111101;
+        }
+        if constexpr (XINT2DirectConnect) {
+            ControlSignals.view8.direction[2] &= 0b11111011;
+        }
+        if constexpr (XINT3DirectConnect) {
+            ControlSignals.view8.direction[2] &= 0b11110111;
+        }
+        if constexpr (XINT4DirectConnect) {
+            ControlSignals.view8.direction[2] &= 0b11101111;
+        }
+        if constexpr (XINT5DirectConnect) {
+            ControlSignals.view8.direction[2] &= 0b11011111;
+        }
+        if constexpr (XINT6DirectConnect) {
+            ControlSignals.view8.direction[2] &= 0b10111111;
+        }
+        if constexpr (XINT7DirectConnect) {
+            ControlSignals.view8.direction[2] &= 0b01111111;
+        }
+        ControlSignals.view32.data = 0;
+        ControlSignals.ctl.hold = 0;
+        ControlSignals.ctl.reset = 0; // put i960 in reset
+        ControlSignals.ctl.backOff = 1;
+        ControlSignals.ctl.ready = 0;
+        ControlSignals.ctl.nmi = 1;
+        ControlSignals.ctl.xint0 = 1;
+        ControlSignals.ctl.xint1 = 1;
+        ControlSignals.ctl.xint2 = 1;
+        ControlSignals.ctl.xint3 = 1;
+        ControlSignals.ctl.xint4 = 1;
+        ControlSignals.ctl.xint5 = 1;
+        ControlSignals.ctl.xint6 = 1;
+        ControlSignals.ctl.xint7 = 1;
+        // select the CH351 bank chip to go over the xbus address lines
+        ControlSignals.ctl.bankSelect = 0;
     }
-    if constexpr (XINT0DirectConnect) {
-        ControlSignals.view8.direction[2] &= 0b11111110;
-    }
-    if constexpr (XINT1DirectConnect) {
-        ControlSignals.view8.direction[2] &= 0b11111101;
-    }
-    if constexpr (XINT2DirectConnect) {
-        ControlSignals.view8.direction[2] &= 0b11111011;
-    }
-    if constexpr (XINT3DirectConnect) {
-        ControlSignals.view8.direction[2] &= 0b11110111;
-    }
-    if constexpr (XINT4DirectConnect) {
-        ControlSignals.view8.direction[2] &= 0b11101111;
-    }
-    if constexpr (XINT5DirectConnect) {
-        ControlSignals.view8.direction[2] &= 0b11011111;
-    }
-    if constexpr (XINT6DirectConnect) {
-        ControlSignals.view8.direction[2] &= 0b10111111;
-    }
-    if constexpr (XINT7DirectConnect) {
-        ControlSignals.view8.direction[2] &= 0b01111111;
-    }
-    ControlSignals.view32.data = 0;
-    ControlSignals.ctl.hold = 0;
-    ControlSignals.ctl.reset = 0; // put i960 in reset
-    ControlSignals.ctl.backOff = 1;
-    ControlSignals.ctl.ready = 0;
-    ControlSignals.ctl.nmi = 1;
-    ControlSignals.ctl.xint0 = 1;
-    ControlSignals.ctl.xint1 = 1;
-    ControlSignals.ctl.xint2 = 1;
-    ControlSignals.ctl.xint3 = 1;
-    ControlSignals.ctl.xint4 = 1;
-    ControlSignals.ctl.xint5 = 1;
-    ControlSignals.ctl.xint6 = 1;
-    ControlSignals.ctl.xint7 = 1;
-    // select the CH351 bank chip to go over the xbus address lines
-    ControlSignals.ctl.bankSelect = 0;
     setupDevices();
 }
 
 CPUKind 
 getInstalledCPUKind() noexcept { 
-    return static_cast<CPUKind>(ControlSignals.ctl.cfg); 
+    if constexpr (UseOldAuxHardwareBoard) {
+        return static_cast<CPUKind>(ControlSignals.ctl.cfg); 
+    } else {
+        return static_cast<CPUKind>((getInputRegister<Port::CTL960>() >> 5) & 0b111);
+    }
 }
 void
 printlnBool(bool value) noexcept {

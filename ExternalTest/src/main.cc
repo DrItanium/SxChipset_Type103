2a073c37fbea980adf85fae42c4c813835c9affe
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-10-14 12:30:14 -0700

    Make the external handler able to support ATMEGA2560 and Uno R4 based targets

--- ExternalTest/src/main.cc
+++ ExternalTest/src/main.cc
@@ -1,12 +1,15 @@
 #include <Arduino.h>
-
+constexpr auto Reset960Pin = 7;
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
+    pinMode(Reset960Pin, OUTPUT);
+    digitalWrite(Reset960Pin, LOW);
     Serial.begin(9600);
     while(!Serial);
-    Serial.println("Startup");
     Serial1.begin(115200);
+
+    digitalWrite(Reset960Pin, HIGH);
 }
 
 void loop() {

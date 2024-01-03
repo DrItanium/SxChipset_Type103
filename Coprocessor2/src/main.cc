52d85a7a524784ed1c91563234776348fbdf5baf
Joshua Scoggins <Theoretically.x64@gmail.com>
2024-01-02 17:07:05 -0800

    Simple program for testing purposes

--- Coprocessor2/src/main.cc
+++ Coprocessor2/src/main.cc
@@ -1,11 +1,23 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
 
 void 
 setup() {
-
+    Wire.begin();
+    SPI.begin();
+    Serial.begin(115200);
+    Serial1.begin(115200);
+    Serial2.begin(115200);
+    Serial3.begin(115200);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
 }
 
 void 
 loop() {
-
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
 }

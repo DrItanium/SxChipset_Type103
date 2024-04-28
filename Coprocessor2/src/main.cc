734bb8ece2ab1ba543c31862f3f9913d4f91e1fc
Joshua Scoggins <Theoretically.x64@gmail.com>
2024-04-28 12:04:44 -0700

    setup a 10mhz source

--- Coprocessor2/src/main.cc
+++ Coprocessor2/src/main.cc
@@ -26,18 +26,49 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <Event.h>
 #include <Logic.h>
 
+void
+setupTimerASources() {
 
+    
+}
+void
+setup10MHzSource() {
+    Event0.set_generator(gen::ccl0_out);
+    Event0.set_user(user::ccl0_event_a);
+    Event0.set_user(user::ccl1_event_a);
+    Logic0.enable = true;
+    Logic0.input0 = in::feedback;
+    Logic0.input1 = in::disable;
+    Logic0.input2 = in::disable;
+    Logic0.output = out::enable;
+    Logic0.truth = 0b0101'0101;
+    Logic0.sequencer = sequencer::jk_flip_flop;
+
+    Logic1.enable = true;
+    Logic1.input0 = in::event_a;
+    Logic1.input1 = in::disable;
+    Logic1.input2 = in::disable;
+    Logic1.output = out::disable;
+    Logic1.sequencer = sequencer::disable;
+    Logic1.truth = 0b0101'0101;
+    Logic0.init();
+    Logic1.init();
+    Event0.start();
+}
 void 
 setup() {
     // as soon as possible, setup the 20MHz clock source
     CCP = 0xD8;
     // internal 20MHz oscillator + enable clkout
     CLKCTRL.MCLKCTRLA = 0b1000'0000;
-    CCP = 0xD8;
+    asm volatile ("nop");
     // make sure that the 20MHz clock runs in standby
     CCP = 0xD8;
     CLKCTRL.OSC20MCTRLA = 0b0000'0010;
-    CCP = 0xD8;
+    asm volatile ("nop");
+
+    setup10MHzSource();
+    Logic::start();
     // then setup the serial port for now, I may disable this at some point
     Serial1.swap(1);
     Serial1.begin(9600);
@@ -47,5 +78,7 @@ setup() {
 
 void 
 loop() {
+    Serial1.println("Donuts");
+    delay(1000);
 }
 
